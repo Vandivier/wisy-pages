e86054b452c568780ce954d12ddb9dc56bb42340
John Vandivier <john.vandivier@halfaker.com>
2017-08-03 21:34:40 -0400

    some caplan notes

--- flash-cards/source/source.js
+++ flash-cards/source/source.js
@@ -17,20 +17,32 @@ var oCourses = {
          "title": "public-choice-2-econ-854-caplan",
          "cards": [
             {
-               "front": "test front",
-               "back": "test back"
+               "front": "Definition of Pareto Efficiency",
+               "back": "No one can be made better off without making someone else worse off.",
+               "otherInfo": {
+                    "unit": "1"
+                }
+            },
+            {
+               "front": "Definition of Kaldor-Hicks Efficiency",
+               "back": " A situation is Kaldor-Hicks efficient if and only if the dollar value of social resources is maximized.",
+               "otherInfo": {
+                    "unit": "1"
+                }
+            },
+            {
+               "front": "Probability of single-voter decisiveness given 21 voters, where non-decisive voters vote in favor of the policy with a probability of .25?",
+               "back": "Using the binomial thoerem, n = 10 and p = .25. The theorem is P(D) = [1/(squareRoot(pi*n))]*(4p-4p^2)^n. The probability of decisiveness is (.75^10)/(squareRoot(10pi)) = .01.",
+               "otherInfo": {
+                    "unit": "1"
+                }
             },
             {
                "front": "test front",
-               "back": "test back"
-            },
-            {
-               "front": "test front",
-               "back": "test back"
-            },
-            {
-               "front": "test front",
-               "back": "test back"
+               "back": "test back",
+               "otherInfo": {
+                    "unit": "1"
+                }
             }
          ]
       }
