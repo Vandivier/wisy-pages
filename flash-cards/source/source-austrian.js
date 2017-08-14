c073fe4058883380fb5e6205f06730c28d3c9f6a
John Vandivier <john.vandivier@halfaker.com>
2017-08-14 10:33:51 -0400

    austrian economics cards;

---
+++ flash-cards/source/source-austrian.js
@@ -0,0 +1,74 @@
+// rename to source to use; it's a way to separate decks;
+var oCards = {
+    "cards": [
+        {
+            "front": "Who is considered the founder of the Austrian School, what is a key concept he contributed, and around what time?",
+            "back": "Carl Menger is considered the founder of the Austrian School. He published Principles in 1871 which contributed to the Marginal Revolution by expressing a subjective theory of value, contrasting with the Classical Labor Theory of Value.",
+            "otherInfo": {
+                "class": "austrian-syl-econ-880-coyne",
+                "unit": "1"
+            }
+            },
+        {
+            "front": "According to Menger, how does money originate?",
+            "back": "Money originates on the market as the most marketable commodity. A marketable commodity is one which is easily exchanged or sold. This was developed by Mises into the Regression Theorem.",
+            "otherInfo": {
+                "class": "austrian-syl-econ-880-coyne",
+                "unit": "1"
+            }
+            },
+        {
+            "front": "What are the 6 prerequisites for Human Action?",
+            "back": "1) A felt uneasiness 2) An imagined preferred state of affairs 3) Belief that sufficient means exist 4) Belief that one's own action using such means will cause or achieve the preferred state of affairs 5) Uncertainty, and particularly the belief that the future is not predetermined 6) Time.",
+            "otherInfo": {
+                "class": "austrian-syl-econ-880-coyne",
+                "unit": "2"
+            }
+            },
+        {
+            "front": "What is Praxeology?",
+            "back": "The study of human action. Economics, Political Science, and Anthropology, for example, are included. Biology and psychology are excluded. Ends are taken as given and the determination of those ends is the purview of Thymology or Psychology rather than Praxeology. Praxeology takes ends as given.",
+            "otherInfo": {
+                "class": "austrian-syl-econ-880-coyne",
+                "unit": "2"
+            }
+            },
+        {
+            "front": "What is Catallactics or Catallaxy?",
+            "back": "The praxeological theory of interaction and exchange.",
+            "otherInfo": {
+                "class": "austrian-syl-econ-880-coyne",
+                "unit": "2"
+            }
+            },
+        {
+            "front": "What is Thymology?",
+            "back": "Thymology is the study of the mental activities of man which determine their actions. Thymology does not take ends as given, but studies the development of ends or preferences. It overlaps largely with psychology.",
+            "otherInfo": {
+                tags: ['Austrian economics']
+            }
+            },
+        {
+            "front": "What are the 6 characteristics of sponteneous orders?",
+            "back": "1) They are the result of purposive human action. 2) They are not the result of human design. That is, they are originally unintended. 3) They can be meaningfully described as orders. That is, there is an identifiable pattern made up of related parts. 4) They are self-reinforcing. 5) The human elements of the order follow rules of conduct which may be formal or informal. 6) Spontaneous orders are abstract objects. Individuals within the order do not need to understand the order or even be aware of its existance.",
+            "otherInfo": {
+                "class": "austrian-syl-econ-880-coyne",
+                "unit": "7"
+            }
+            },
+        {
+            "front": "What is the difference between subjectivism and radical subjectivism?",
+            "back": "Subjectivism has been the main theory of value in economics since the marginal revolution in the late 1800s. It holds that value is subjectively determined rather than based, for example, on the input labor required. Radical subjectivism, in contrast, is a kaleidic theory based on the work of the Austrian Ludwige Lachmann and developed by the Post-Keynesian George Shackle from the mid to late 1900s. This theory holds that individuals and markets may not predictably be heading toward an equilibrium state. It argues this by identifying 1) uncertainty as non-endogenizable and 2) ends or preferences as dynamic, and not necessarily moving in any particular direction.",
+            "otherInfo": {
+                tags: ['radical subjectivism']
+            }
+            },
+        {
+            "front": "What are three traits peculiar to Austrian Economics?",
+            "back": "1) The school's approach to process and knowledge. Equilibrium is not viewed as a thing to be obtained and instead the process of equilibration is emphasized. This process is seen as proceeding according to individual action. This principle is identified as methodological individualism. Multi-specific knowledge or belief sets are key in this view, as is the role of uncertainty. 2) The school's emphasis on entrepreneurship. Mises and Hayek emphasized the role of the entrepreneus as an arbitrageur. Kirzner emphasized the entrepreneur taking risk under uncertainty, aided by alertness. Schumpeter emphasized the entrepreneur-as-inventor. Neoclassical theory views an entrepreneur as a search specialist. Austrians dominate the field of entrepreneurial economic theory, in a niche way. 3) The multi-specific nature of the world. Austrians led the adoption of heterogeneity into analysis, but mainstream economics has picked that up. What they have yet to pick up, except game theory, is the critically multi-specific nature of capital, goods, and the world. Even game theory owes to the Austrians as it was founded by von Neumann and Morgenstern, where Austrians influenced them both. Mises, for example, was Morgenstern's doctoral advisor.",
+            "otherInfo": {
+                tags: ['Austrian economics']
+            }
+            }
+   ]
+}
