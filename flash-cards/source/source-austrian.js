8352c58ce325cfd29f6d0e9305a4b5dc649922b5
John Vandivier <john.vandivier@halfaker.com>
2017-08-15 10:04:40 -0400

    austrian card updates;

--- flash-cards/source/source-austrian.js
+++ flash-cards/source/source-austrian.js
@@ -43,14 +43,14 @@ var oCards = {
             },
         {
             "front": "What is Thymology?",
-            "back": "Thymology is the study of the mental activities of man which determine their actions. Thymology does not take ends as given, but studies the development of ends or preferences. It overlaps largely with psychology.",
+            "back": "Thymology is the study of the antecedent of human action. It is the study of mental activities which determine individual ends and preferences. Thymology does not take ends as given, but studies the development of ends and preferences. It overlaps largely with psychology, and may overlap with biology and other such fields.",
             "otherInfo": {
-                tags: ['Austrian economics']
+                tags: ["Austrian economics"]
             }
             },
         {
             "front": "What are the 6 characteristics of sponteneous orders?",
-            "back": "1) They are the result of purposive human action. 2) They are not the result of human design. That is, they are originally unintended. 3) They can be meaningfully described as orders. That is, there is an identifiable pattern made up of related parts. 4) They are self-reinforcing. 5) The human elements of the order follow rules of conduct which may be formal or informal. 6) Spontaneous orders are abstract objects. Individuals within the order do not need to understand the order or even be aware of its existance.",
+            "back": "1) They are the result of purposive human action. 2) They are not the result of human design. That is, they are originally unintended. 3) They can be meaningfully described as orders. That is, there is an identifiable pattern made up of related parts. 4) They are self-reinforcing. 5) The human elements of the order follow rules of conduct which may be formal or informal. 6) Spontaneous orders are abstract objects. They do not need to be understood or invoked to operate. Individuals within the order may not even be aware of its existance.",
             "otherInfo": {
                 "class": "austrian-syl-econ-880-coyne",
                 "unit": "7"
@@ -58,16 +58,24 @@ var oCards = {
             },
         {
             "front": "What is the difference between subjectivism and radical subjectivism?",
-            "back": "Subjectivism has been the main theory of value in economics since the marginal revolution in the late 1800s. It holds that value is subjectively determined rather than based, for example, on the input labor required. Radical subjectivism, in contrast, is a kaleidic theory based on the work of the Austrian Ludwige Lachmann and developed by the Post-Keynesian George Shackle from the mid to late 1900s. This theory holds that individuals and markets may not predictably be heading toward an equilibrium state. It argues this by identifying 1) uncertainty as non-endogenizable and 2) ends or preferences as dynamic, and not necessarily moving in any particular direction.",
+            "back": "Subjectivism is a mainline theory of value which emerged during the marginal revolution of the late 1800s. It holds that value is subjectively determined, in contrast to the Classical Labor Theory of Value. Radical subjectivism, in contrast, is a kaleidic, or non-equilibrium, theory of economic process. It is based on the work of the Austrian economist Ludwig Lachmann and was developed by the Post-Keynesian economist George Shackle during the mid to late 1900s. This theory holds that individuals and markets may not predictably be heading toward an equilibrium state. It argues this by identifying 1) uncertainty as non-endogenizable and 2) ends, preferences, and optima as dynamic in a directionally ambiguous or uncertain way, emphasizing the possibility of a discrepancy between variables ex ante and ex post taking an action or making a decision.",
             "otherInfo": {
-                tags: ['radical subjectivism']
+                tags: ["radical subjectivism"]
             }
             },
         {
             "front": "What are three traits peculiar to Austrian Economics?",
             "back": "1) The school's approach to process and knowledge. Equilibrium is not viewed as a thing to be obtained and instead the process of equilibration is emphasized. This process is seen as proceeding according to individual action. This principle is identified as methodological individualism. Multi-specific knowledge or belief sets are key in this view, as is the role of uncertainty. 2) The school's emphasis on entrepreneurship. Mises and Hayek emphasized the role of the entrepreneus as an arbitrageur. Kirzner emphasized the entrepreneur taking risk under uncertainty, aided by alertness. Schumpeter emphasized the entrepreneur-as-inventor. Neoclassical theory views an entrepreneur as a search specialist. Austrians dominate the field of entrepreneurial economic theory, in a niche way. 3) The multi-specific nature of the world. Austrians led the adoption of heterogeneity into analysis, but mainstream economics has picked that up. What they have yet to pick up, except game theory, is the critically multi-specific nature of capital, goods, and the world. Even game theory owes to the Austrians as it was founded by von Neumann and Morgenstern, where Austrians influenced them both. Mises, for example, was Morgenstern's doctoral advisor.",
             "otherInfo": {
-                tags: ['Austrian economics']
+                tags: ["Austrian economics"]
+            }
+            },
+        {
+            "front": "What do ex ante and ex post refer to? Why does it matter?",
+            "back": "Ex post, or ex-post, means 'after the fact.' Ex post values are historical data, or actual values. Ex ante means 'before the fact' and it refers to estimated values, expectations, forecasts, and so on. The difference between ex post and ex ante values goes by various labels including error, unexpected shift, and exogenous shock.",
+            "otherInfo": {
+                tags: ["Austrian economics", "radical subjectivism"],
+                urls: ["http://www.investopedia.com/terms/e/exante.asp", "http://www.investopedia.com/terms/e/expost.asp"]
             }
             }
    ]
