0e8663461b8e0ca5f72a7a8ca75abe35b0184d8d
Vandivier <vandivier_john@yahoo.com>
2017-12-04 00:48:37 -0500

    meritorious goods and qualities of student

--- flash-cards/source/source.js
+++ flash-cards/source/source.js
@@ -9,7 +9,8 @@ var oCards = {
             "back": "Two explanations: Signaling and Indirect Learning [TODO: details]",
             "otherInfo": {
                 "class": "economics-of-education-econ-895-caplan",
-                "unit": "ideological turing test"
+                "unit": "ideological turing test",
+                "pageref": "TODO"
             }
             },
         {
@@ -17,7 +18,8 @@ var oCards = {
             "back": "TODO",
             "otherInfo": {
                 "class": "economics-of-education-econ-895-caplan",
-                "unit": "ideological turing test"
+                "unit": "ideological turing test",
+                "pageref": "TODO"
             }
             },
         {
@@ -25,7 +27,8 @@ var oCards = {
             "back": "Federal law effectively prohibits work for kids under 14, except in family businesses, farming, newspaper delivery, and performing arts.",
             "otherInfo": {
                 "class": "economics-of-education-econ-895-caplan",
-                "unit": "13"
+                "unit": "Week 13",
+                "pageref": "TODO"
             }
             },
         {
@@ -33,7 +36,35 @@ var oCards = {
             "back": "Child labor presents a double standard compared to education. 1. It’s OK for kids to be bored and uncomfortable at school, but not at work. 2. It’s OK for kids to devote every spare minute to sports, music, drama, or chess, but not work. 3. Employers 'exploit' kids if they pay them a low wage, but schools don’t 'exploit' students by paying them a negative wage (charging tuition).",
             "otherInfo": {
                 "class": "economics-of-education-econ-895-caplan",
-                "unit": "13"
+                "unit": "Week 13",
+                "pageref": "TODO"
+            }
+            },
+        {
+            "front": "What are Caplan's three requirements for education as a meritorious good?",
+            "back": "The three requirements for education as a merit good identified in the text include worthy content, skillful pedagogy, and eager students.",
+            "otherInfo": {
+                "class": "economics-of-education-econ-895-caplan",
+                "unit": "Chapter 9",
+                "pageref": "TODO"
+            }
+            },
+        {
+            "front": "What are Caplan's four qualities of student?",
+            "back": "Poor, fair, good, and excellent. A poor student fits the profile of a typical high school dropout. A fair student fits the profile of a typical high school graduate who does not try college. A good student fits the profile of a typical 4 year degree holder who did not try graduate education. An excellent student fits the profile of the typical Master's degree holder.",
+            "otherInfo": {
+                "class": "economics-of-education-econ-895-caplan",
+                "unit": "Chapter 5",
+                "pageref": "145"
+            }
+            },
+        {
+            "front": "A typical Ph.D. holder would fall into which of Caplan's qualities of student?",
+            "back": "None of the above. The excellent student fits the profile of a typical Master's degree holder. A Ph.D. holder is a significantly different profile. The Ph.D. education is an important edge case which Caplan does not deal with. One might guess at outcomes for this class of person by extrapolating trends among the lower classes, or by looking at outcomes for an above average excellent student.",
+            "otherInfo": {
+                "class": "economics-of-education-econ-895-caplan",
+                "unit": "Chapter 5",
+                "pageref": "145"
             }
             }
    ]
